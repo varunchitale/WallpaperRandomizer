935139618aed11e9afa89db6e38bcf9700862b70
Varun Chitale <varunchitale@gmail.com>
2018-02-12 15:41:05 +0530

    Ugh

--- changeBG.py
+++ changeBG.py
@@ -6,7 +6,6 @@ import requests
 
 url = 'https://source.unsplash.com/random/1600x900'
 response = requests.get(url, stream=True)
-os.remove('img.jpg')
 with open('img.jpg', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
@@ -14,4 +13,5 @@ del response
 
 
 
-os.system('gsettings set org.gnome.desktop.background picture-uri \'file:PATH/TO?WORKING?DIRECTORY/img.jpg\'')
+os.system("gsettings set org.gnome.desktop.background \
+		picture-uri 'file:/home/prdtr/Desktop/Projects/bgRandom/img.jpg'")
