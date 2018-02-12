8aa50128dee194d073313caf0e568c1f35ef3e35
Varun Chitale <varunchitale@gmail.com>
2018-02-12 15:28:58 +0530

    Fixed a bug in path

--- changeBG.py
+++ changeBG.py
@@ -14,4 +14,4 @@ del response
 
 
 
-os.system('gsettings set org.gnome.desktop.background picture-uri \'file:~./img.jpg\'')
+os.system('gsettings set org.gnome.desktop.background picture-uri \'file:PATH/TO?WORKING?DIRECTORY/img.jpg\'')
