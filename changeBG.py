2bc3f7fbe8ddb472dbcd0dd79c9809ebccb1b8bf
Varun Chitale <varunchitale@gmail.com>
2018-02-12 16:05:52 +0530

    An even more OS independent script
    Removed absolute path. Can be run from any source.

--- changeBG.py
+++ changeBG.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 #Background Randomizer Script
 import shutil
-import os
+import os,sys
 import requests
+
+pathname = os.path.dirname(sys.argv[0])        
+curr_dir = os.path.abspath(pathname)
+
+
 
 url = 'https://source.unsplash.com/random/1600x900'
 response = requests.get(url, stream=True)
-with open('img.jpg', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
+
+f = open('%s/img.jpg'%curr_dir, 'wb')
+shutil.copyfileobj(response.raw, f)
 del response
 
 
 
 
 os.system("gsettings set org.gnome.desktop.background \
-		picture-uri 'file:/home/prdtr/Desktop/Projects/bgRandom/img.jpg'")
+		picture-uri 'file:%s/img.jpg'"%curr_dir)
