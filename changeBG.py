24c1c0f9cb5cb1658b88b699c986f12f121a6995
Varun Chitale <varunchitale@gmail.com>
2018-02-12 15:07:55 +0530

    Init
    Init

---
+++ changeBG.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#Background Randomizer Script
+import shutil
+import os
+import requests
+
+url = 'https://source.unsplash.com/random/1600x900'
+response = requests.get(url, stream=True)
+os.remove('img.jpg')
+with open('img.jpg', 'wb') as out_file:
+    shutil.copyfileobj(response.raw, out_file)
+del response
+
+
+
+
+os.system('gsettings set org.gnome.desktop.background picture-uri \'file:/home/prdtr/Desktop/Projects/bgRandom/img.jpg\'')
